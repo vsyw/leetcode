486ced828f4c7d750865897c46b92fdb9dec539f
victor wang <victor.sy_wang@htc.com>
2016-06-15 11:35:12 +0800

    add leetcode no.189

---
+++ 189.Rotate Array/rotate-array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+  for(var i = 0; i < k; i++) {
+    nums.unshift(nums.pop());
+  }
+};

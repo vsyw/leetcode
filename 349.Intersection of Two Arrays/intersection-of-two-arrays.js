1983db7d7413283ece387c52e6f7f24e456bbf0e
victor wang <iwantooxxoox@gmail.com>
2016-06-29 11:45:24 +0800

    add leetcode no.349 O(n) solution

--- 349.Intersection of Two Arrays/intersection-of-two-arrays.js
+++ 349.Intersection of Two Arrays/intersection-of-two-arrays.js
@@ -6,17 +6,33 @@
 
 
 // O(n^2) solution
+// var intersection = function(nums1, nums2) {
+//   let output = new Set();
+//   for (let i = 0; i < nums1.length; i++) {
+//     for (let j = 0; j < nums2.length; j++) {
+//       if (nums1[i] === nums2[j]) {
+//         output.add(nums2[j]);
+//       }
+//     }
+//   }
+//   output = [...output];
+//   return output;
+// };
+
+// O(n) solution
 var intersection = function(nums1, nums2) {
-  let output = new Set();
+  let set1 = new Set();
   for (let i = 0; i < nums1.length; i++) {
-    for (let j = 0; j < nums2.length; j++) {
-      if (nums1[i] === nums2[j]) {
-        output.add(nums2[j]);
-      }
+    set1.add(nums1[i]);
+  }
+  console.log(set1);
+  let output = new Set();
+  for (let j = 0; j < nums2.length; j++) {
+    if (set1.has(nums2[j])) {
+      output.add(nums2[j]);
     }
   }
-  output = [...output];
-  return output;
-};
+  return [...output];
+}
 
 console.log(intersection([1,2,2,1], [2,2, 1]));

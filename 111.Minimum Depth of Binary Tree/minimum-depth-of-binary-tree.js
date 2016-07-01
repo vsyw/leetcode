a402f82713b60f3be3f6352ebb337f0bc62dec4c
victor wang <iwantooxxoox@gmail.com>
2016-07-01 11:15:21 +0800

    add leetcode no.111 with recursive solution

---
+++ 111.Minimum Depth of Binary Tree/minimum-depth-of-binary-tree.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+var minDepth = function(root) {
+  if (!root) return 0;
+  if (root.left !== null && root.right !== null) {
+    return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+  } else if (root.left == null) {
+    return minDepth(root.right) + 1;
+  } else if (root.right == null) {
+    return minDepth(root.left) + 1;
+  } return 1;
+};

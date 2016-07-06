7cad21f9a26e77803a612692c9da5158c966603e
victor wang <iwantooxxoox@gmail.com>
2016-07-06 11:32:27 +0800

    correct leetcode no.165 algorithm:

--- 165. Compare Version Numbers/compare-version-numbers.js
+++ 165. Compare Version Numbers/compare-version-numbers.js
@@ -4,19 +4,17 @@
  * @return {number}
  */
 var compareVersion = function(version1, version2) {
-  const version1_int = parseInt(version1.split('.')[0]);
-  const version1_dec = parseInt(version1.split('.')[1]);
+  const v1 = version1.split('.');
+  const v2 = version2.split('.');
 
-  const version2_int = parseInt(version2.split('.')[0]);
-  const version2_dec = parseInt(version2.split('.')[1]);
-
-  if (version1_int > version2_int) return 1;
-  else if (version1_int < version2_int) return -1;
-  else if (version1_int === version2_int) {
-    if (version1_dec > version2_dec || !version2_dec) return 1;
-    if (version1_dec < version2_dec || !version1_dec) return -1;
-    return 0;
-  } 
+  for (let i = 0; i < Math.max(v1.length, v2.length); i++) {
+    if ((parseInt(v1[i], 10) > parseInt(v2[i], 10)) || (parseInt(v1[i], 10) && !v2[i])) {
+      return 1;
+    } else if ((parseInt(v1[i], 10) < parseInt(v2[i], 10)) || (parseInt(v2[i], 10) && !v1[i])) {
+      return -1;
+    }
+  }
+  return 0;
 };
 
-console.log(compareVersion('1.1', '1'));
+console.log(compareVersion('1.0.1', '1.0.1.0'));

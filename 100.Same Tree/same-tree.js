df69a7fc293f11a9dcf2c870ad5f72bd7b3fbeeb
victor wang <iwantooxxoox@gmail.com>
2016-07-10 23:49:37 +0800

    add leetcode 114 without solution yet

--- 100.Same Tree/same-tree.js
+++ 100.Same Tree/same-tree.js
@@ -14,3 +14,5 @@ var isSameTree = function(p, q) {
   if (p === null || q === null) return (p === q);
   return ((p.val === q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
 };
+
+

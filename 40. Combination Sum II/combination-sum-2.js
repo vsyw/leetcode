21d43b571ce930dbd05493ae0342e01b90ee2377
victor wang <iwantooxxoox@gmail.com>
2016-07-14 11:51:24 +0800

    add leetcode no.22 solution

---
+++ 40. Combination Sum II/combination-sum-2.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+function backtrack(ans, comb, candidates, target, start) {
+  if (target < 0) return;
+  if (target === 0) {
+    ans.push(comb.slice());
+    return;
+  }
+  for (let i = start; i < candidates.length; i++) {
+    // if (i > start && candidates[i] === candidates[i - 1]) continue;
+    comb.push(candidates[i]);
+    backtrack(ans, comb, candidates, target - candidates[i], i + 1);
+    comb.pop();
+  }
+} 
+
+var combinationSum2 = function(candidates, target) {
+  let ans = [];
+  let comb = [];
+  candidates.sort((a, b) => {
+    return a <= b ? -1 : 1;
+  });
+  console.log(candidates);
+  backtrack(ans, comb, candidates, target, 0);
+  return ans;
+};
+
+console.log(combinationSum2([10, 1, 2, 7, 6, 1, 5], 8));

40376fa35d104e49ad9bdac5790c770dcbbdab38
victor wang <iwantooxxoox@gmail.com>
2016-07-17 21:36:06 +0800

    add leetcode no.78 solution

--- 90.Subsets-2/subsets-2.js
+++ 90.Subsets-2/subsets-2.js
@@ -2,6 +2,35 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+function backtrack(ans, set, nums, start) {
+  // if (start === nums.length) {
+  //   ans.push(set.slice());
+  //   return;
+  // }
+  ans.push(set.slice());
+  for (let i = start; i < nums.length; i++) {
+    set.push(nums[i]);
+    backtrack(ans, set.slice(), nums, i + 1);
+    set.pop();
+  }
+}
+
 var subsetsWithDup = function(nums) {
-    
+  nums.sort((a, b) => {
+    return a - b;
+  });
+  let ans = [];
+  let set = [];
+  backtrack(ans, set, nums, 0);
+  return ans;
 };
+
+console.log(subsetsWithDup([1, 2]));
+
+// [2,3,4,5]
+
+
+// 1,2
+// 1, ,
+//  ,2,
+//  , , 

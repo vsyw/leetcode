d3ad2c39829c7a42fc16de56ccc5d46c16639dc5
victor wang <iwantooxxoox@gmail.com>
2016-07-20 15:23:40 +0800

    add sorting folder and bubble sort implementation

---
+++ Sorting/bubble-sort.js
@@ -0,0 +1,12 @@
+function bubble (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubble([32, 4, 166, 34, 99]));

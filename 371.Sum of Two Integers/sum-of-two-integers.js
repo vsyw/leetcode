886b519aa9d60c90311065927a2326a35b30e5df
victor wang <iwantooxxoox@gmail.com>
2016-07-25 10:48:37 +0800

    add leetcode no.371 sum of two integers recusive solution

--- 371.Sum of Two Integers/sum-of-two-integers.js
+++ 371.Sum of Two Integers/sum-of-two-integers.js
@@ -4,21 +4,21 @@
  * @return {number}
  * Good referrence from: http://blog.csdn.net/itismelzp/article/details/49621741
  */
-const getSum = function (a, b) {
-  let carry;
-  while (b) {
-    carry = (a & b) << 1;
-    a = a ^ b;
-    b = carry;
-  }
-  return a;
-};
-
 // const getSum = function (a, b) {
-//   if (b === 0) return a;
-//   let carry = (a & b) << 1;
-//   a = a ^ b;
-//   return getSum(a, carry);
+//   let carry;
+//   while (b) {
+//     carry = (a & b) << 1;
+//     a = a ^ b;
+//     b = carry;
+//   }
+//   return a;
+// };
+
+const getSum = function (a, b) {
+  if (b === 0) return a;
+  let carry = (a & b) << 1;
+  a = a ^ b;
+  return getSum(a, carry);
 // }
 
 console.log(getSum(10, -3));

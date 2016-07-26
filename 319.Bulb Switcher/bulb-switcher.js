ff4a00dc50cd910816fbe5f1237eae7e06502432
victor wang <iwantooxxoox@gmail.com>
2016-07-26 13:14:32 +0800

    update no.319

--- 319.Bulb Switcher/bulb-switcher.js
+++ 319.Bulb Switcher/bulb-switcher.js
@@ -1,9 +1,13 @@
 /**
  * @param {number} n
  * @return {number}
+ * 設有 n 個燈泡，第 i 個燈泡 on or off 取決於小於等於 n 的數字有幾個可以整除 i
+ * 基數個的話是 on，偶數個是 off
+ * 然後除了完全平方數，其他的數字因數都是成對出現（偶數）
+ * 所以這題變成要算，小於等於 n 的數字中，有多少完全平方數 （bulb on)
  */
 const bulbSwitch = function (n) {
-  return  Math.floor(Math.sqrt(n));
+  return Math.floor(Math.sqrt(n));
 };
 
 console.log(bulbSwitch(36));

2c394f1eaf1b694723eafffcdcec6d80c43cd795
victor wang <iwantooxxoox@gmail.com>
2016-08-05 11:41:16 +0800

    add leetcode no.208 implement trie with es5 and es6 solutions

---
+++ 208.Implement Trie (Prefix Tree)/implement-trie-es6.js
@@ -0,0 +1,48 @@
+class TrieNode {
+  
+  constructor() {
+    this.isWord = 0;
+    this.children = {};
+  }
+
+  insert(word) {
+    if (word.length === 0) {
+      return;
+    }
+    let p = this;
+    for (let i = 0; i < word.length; i++) {
+      if (!p.children.hasOwnProperty(word[i])) {
+        p.children[word[i]] = new TrieNode();
+      }
+      p = p.children[word[i]];
+    }
+    p.isWord++;
+  }
+
+  search(word) {
+    let p = this;
+    for (let i = 0; i < word.length; i++) {
+      if (!p.children.hasOwnProperty(word[i])) {
+        return false;
+      }
+      p = p.children[word[i]];
+    }
+    // console.log('show ', p.isWord);
+    return p.isWord > 0;
+  }
+
+  startsWith(prefix) {
+    let p = this;
+    for (let i = 0; i < prefix.length; i++) {
+      if (!p.children.hasOwnProperty(prefix[i])) {
+        return false;
+      }
+      p = p.children[prefix[i]];
+    }
+    return true;
+  }
+}
+
+const head = new TrieNode();
+head.insert('fuck');
+console.log(head.search('fuc'));
--- 208.Implement Trie (Prefix Tree)/implement-trie.js
+++ 208.Implement Trie (Prefix Tree)/implement-trie.js
@@ -2,13 +2,13 @@
  * @constructor
  * Initialize your data structure here.
  */
-var TrieNode = function() {
-  this.isWord = false;
+var TrieNode = function () {
+  this.isWord = 0;
   this.children = {};
 };
 
-var Trie = function() {
-    this.root = TrieNode();
+var Trie = function () {
+  this.root = new TrieNode();
 };
 
 /**
@@ -16,8 +16,18 @@ var Trie = function() {
  * @return {void}
  * Inserts a word into the trie.
  */
-Trie.prototype.insert = function(word) {
-    
+Trie.prototype.insert = function (word) {
+  if (word.length === 0) {
+    return;
+  }
+  let p = this.root;
+  for (let i = 0; i < word.length; i++) {
+    if (!p.children.hasOwnProperty(word[i])) {
+      p.children[word[i]] = new TrieNode();
+    }
+    p = p.children[word[i]];
+  }
+  p.isWord++;
 };
 
 /**
@@ -25,8 +35,16 @@ Trie.prototype.insert = function(word) {
  * @return {boolean}
  * Returns if the word is in the trie.
  */
-Trie.prototype.search = function(word) {
-  
+Trie.prototype.search = function (word) {
+  let p = this.root;
+  for (let i = 0; i < word.length; i++) {
+    if (!p.children.hasOwnProperty(word[i])) {
+      return false;
+    }
+    p = p.children[word[i]];
+  }
+  // console.log('show ', p.isWord);
+  return p.isWord > 0;
 };
 
 /**
@@ -35,8 +53,15 @@ Trie.prototype.search = function(word) {
  * Returns if there is any word in the trie
  * that starts with the given prefix.
  */
-Trie.prototype.startsWith = function(prefix) {
-    
+Trie.prototype.startsWith = function (prefix) {
+  let p = this.root;
+  for (let i = 0; i < prefix.length; i++) {
+    if (!p.children.hasOwnProperty(prefix[i])) {
+      return false;
+    }
+    p = p.children[prefix[i]];
+  }
+  return true;
 };
 
 /**
@@ -45,3 +70,34 @@ Trie.prototype.startsWith = function(prefix) {
  * trie.insert("somestring");
  * trie.search("key");
  */
+
+var trie = new Trie();
+trie.insert('abc');
+console.log(trie.search('abc'));
+console.log(trie.search('ab'));
+trie.insert('ab');
+console.log(trie.search('ab'));
+trie.insert('ab');
+console.log(trie.search('ab'));
+
+// console.log(trie.startsWith('a'));
+// trie.insert('fuckyou');
+// trie.insert('shit');
+// trie.insert('test');
+// console.log(trie.search('fu'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

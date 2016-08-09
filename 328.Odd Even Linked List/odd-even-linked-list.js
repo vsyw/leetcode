e4fa4239c40b90e0143203f0ad54670f5bbdff23
victor wang <iwantooxxoox@gmail.com>
2016-08-09 12:11:58 +0800

    add leetcode no.328 odd-even-linked-list with the two pointer solution

---
+++ 328.Odd Even Linked List/odd-even-linked-list.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const oddEvenList = function (head) {
+  if (head === null) return null;
+  let odd = head;
+  let even = head.next;
+  const evenHead = even;
+
+  while (even !== null && even.next !== null) {
+    odd.next = odd.next.next;
+    even.next = even.next.next;
+    odd = odd.next;
+    even = even.next;
+  }
+  odd.next = evenHead;
+  return head;
+};

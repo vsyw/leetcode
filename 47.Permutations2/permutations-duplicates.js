be7a200c6518e8d997b2cd75511a559f1333ca72
victor wang <iwantooxxoox@gmail.com>
2016-08-16 15:50:14 +0800

    add leetcode  47.Permutations2/permutations-duplicates.js

--- 47.Permutations2/permutations-duplicates.js
+++ 47.Permutations2/permutations-duplicates.js
@@ -9,16 +9,20 @@ function permuteRecursive(begin, nums, result) {
   }
   for (let i = begin; i < nums.length; i++) {
     // swap
+    if (i !== begin && nums[i] === nums[begin]) {
+      continue;
+    }
     [nums[begin], nums[i]] = [nums[i], nums[begin]];
     permuteRecursive(begin + 1, nums.slice(), result);
-    [nums[begin], nums[i]] = [nums[i], nums[begin]];
+    // [nums[begin], nums[i]] = [nums[i], nums[begin]];
   }
 }
 
-const permute = function (nums) {
+const permuteUnique = function (nums) {
   const result = [];
+  nums = nums.sort((a, b) => a - b);
   permuteRecursive(0, nums, result);
   return result;
 };
 
-console.log(permute([1, 2, 3]));
+console.log(permuteUnique([1, 1, 2, 2]));

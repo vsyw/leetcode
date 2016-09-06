dd75a0b89d2a7e64d5a7d4db36b6617f31f11937
victor wang <iwantooxxoox@gmail.com>
2016-09-06 12:15:53 +0800

    add leetcode no.39

---
+++ 39.Combination Sum/combination-sum.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+function backtrack(candidates, target, index, tmpRes, res) {
+  if (target === 0) {
+    res.push(tmpRes);
+    return;
+  }
+  if (target < 0) return;
+
+  for (let i = index; i < candidates.length; ++i) {
+    if (i > index && candidates[i] === candidates[i - 1]) continue;
+    if (candidates[i] <= target) {
+      tmpRes.push(candidates[i]);
+      console.log(tmpRes);
+      console.log(i);
+      backtrack(candidates, target - candidates[i], i, tmpRes.slice(), res);
+      tmpRes.pop();
+    }
+  }
+}
+
+const combinationSum = function (candidates, target) {
+  candidates.sort((a, b) => a - b);
+  let sum = 0;
+  const tmpRes = [];
+  const res = [];
+  backtrack(candidates, target, 0, tmpRes, res);
+  return res;
+};
+
+console.log(combinationSum([2, 3, 7], 7));

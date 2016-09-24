25b813ff4c8954f9fe693decd46f85e8bd9331bd
victor <iwantooxxoox@gmail.com>
2016-09-24 23:48:33 +0800

    add the explanation of the approach

--- 198.House Robber/house-rober.js
+++ 198.House Robber/house-rober.js
@@ -2,6 +2,7 @@
  * @param {number[]} nums
  * @return {number}
  */
+ //it is similar to dp, you could consider a as previous max num at even, b as previous max num at odd.
 var rob = function(nums) {
   let a = 0;
   let b = 0;

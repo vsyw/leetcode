777a6418bece5aa4a0b63008f4551143799216f4
victor wang <iwantooxxoox@gmail.com>
2016-09-29 23:29:40 +0800

    127.Word Ladder/word-laddar.js

--- 118.Pascal's Triangle/pascal's-triangle-consice.js
+++ 118.Pascal's Triangle/pascal's-triangle-consice.js
@@ -7,7 +7,6 @@ const generate = function (numRows) {
   for (let i = 0; i < numRows; i++) {
     res.push([]);
     res[i][0] = res[i][i] = 1;
-    // res[i][i] = 1;
     for (let j = 1; j < i; j++) {
       res[i][j] = res[i - 1][j - 1] + res[i - 1][j];
     }

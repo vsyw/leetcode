1336330c984e7bcd615c84a0067c60f124e6ca8b
victor wang <iwantooxxoox@gmail.com>
2016-10-02 17:00:37 +0800

    update merge-sort

--- Sorting/merge-sort.js
+++ Sorting/merge-sort.js
@@ -2,10 +2,17 @@
  * @param {array} arr
  */
 function merge(left, right, arr) {
-  let l = 0, r = 0;
+  let l = 0, r = 0, k = 0;
   while (l < left.length && r < right.length) {
-    
+    if (left[l] < right[r]) {
+      arr[k++] = left[l++];
+    } else {
+      arr[k++] = right[r++];
+    }
   }
+  while (l < left.length) arr[k++] = left[l++];
+  while (r < right.length) arr[k++] = right[r++];
+  // console.log(arr);
 }
 
 function mergeSort(arr) {
@@ -18,5 +25,5 @@ function mergeSort(arr) {
   merge(left, right, arr);
   console.log(arr);
 }
-
-mergeSort([1, 2, 3, 4, 5, 6]);
+// merge([1, 4], [2, 6], []);
+mergeSort([7, 2, 5, 4, 6]);

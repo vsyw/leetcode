e4165e698339a8e70afabb40b4e33f3ce3dcbce6
victor wang <iwantooxxoox@gmail.com>
2016-10-12 13:26:16 +0800

    add leetcode 107. Binary Tree Level Order Traversal II with reverse the output from top-down BFS answer

---
+++ 107.Binary Tree Level Order Traversal II/binary-tree-level-order-traversal-2.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+const levelOrderBottom = function (root) {
+  if (root === null) return [];
+  const q = [];
+  const level = 1;
+  const res = [];
+  let curLevel = level;
+
+  q.push([level, root]);
+  let tmp = [];
+  
+  while (q.length > 0) {
+    const node = q.shift();
+    if (node[1] && node[1].left !== null) q.push([node[0] + 1, node[1].left]);
+    if (node[1] && node[1].right !== null) q.push([node[0] + 1, node[1].right]);
+    
+    if (node[0] === curLevel) {
+      tmp.push(node[1].val);
+    } else {
+      res.push(tmp.slice());
+      curLevel = node[0];
+      tmp = [node[1].val];
+    }
+  }
+  return [...res, tmp].reverse();
+};

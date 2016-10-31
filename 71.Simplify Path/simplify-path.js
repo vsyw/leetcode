ce8642840759b0e675161a28577cfd7b15800a14
victor wang <iwantooxxoox@gmail.com>
2016-10-31 15:06:29 +0800

    add leetcode no.64.Minimum Path Sum

---
+++ 71.Simplify Path/simplify-path.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} path
+ * @return {string}
+ */
+const simplifyPath = function (path) {
+  const p = path.split('/');
+  const stack = [];
+  for (let i = 0; i < p.length; i++) {
+    if (p[i] === '.' || p[i] === '') continue;
+    else if (p[i] === '..') stack.pop();
+    else {
+      stack.push(`/${p[i]}`);
+    }
+  }
+  if (stack.length === 0) return '/';
+  return stack.join('');
+  // console.log(stack.join(''));
+  // return p;
+};
+
+console.log(simplifyPath('/home//foo/'));
+
